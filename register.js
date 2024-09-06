c505dfa4522479687859c70fed8c03ce6f4b9151
Tallent <madlopatalent54@gmail.com>
2024-09-06 20:09:59 +0200

    Initial commit

---
+++ register.js
@@ -0,0 +1,16 @@
+document.getElementById('registerForm').addEventListener('submit', function(e) {
+    e.preventDefault(); // Prevent form submission
+
+    const username = document.getElementById('regUsername').value;
+    const password = document.getElementById('regPassword').value;
+
+    // Check if the user already exists
+    if (localStorage.getItem(username)) {
+        document.getElementById('errorMessage').style.display = 'block';
+    } else {
+        // Store new user credentials in localStorage
+        localStorage.setItem(username, password);
+        document.getElementById('regMessage').style.display = 'block';
+        document.getElementById('errorMessage').style.display = 'none';
+    }
+});
